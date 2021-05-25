0230452236b33fc4302553972dd93d36901e368a
FlutLab.io <>
2021-05-25 03:41:57 +0000

    Autosave

--- lib/pages/third_page.dart
+++ lib/pages/third_page.dart
@@ -24,45 +24,64 @@ class _ThirdPageState extends State<ThirdPage> {
     return Material(
       child: ConstrainedBox(
         constraints: BoxConstraints(maxWidth: 300, minWidth: 200),
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 80.0,),
-            Container(
-              color: Colors.purple,
-              height: 200,
-              width: 350,
-            ),// Contenedor 1
-            SizedBox(height: 20.0,),
-            Container(
-              height: 200,
-              width: 250,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30.0),
-                gradient: LinearGradient(
-                  colors: [
-                    Colors.yellow,
-                    Colors.yellow[300],
-                    Colors.yellow[200],
-                  ],
-                  begin: Alignment.bottomLeft,
-                  end: Alignment.topRight,
-                ),// Gradiente
-              ),// Box Decoration
-              child: Container(
-                padding: EdgeInsets.only(top: 35.0, bottom: 16),
-                child: Text(
-                  'Najera 6J',
-                  textAlign: TextAlign.center, 
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),// Texto
-              ),// Contenedor hijo
-            ),// Contenedor 2
-          ],// Hijos
-        ),// Columna
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 80.0,),
+              Container(
+                color: Colors.purple,
+                height: 200,
+                width: 350,
+              ),// Contenedor 1
+              SizedBox(height: 20.0,),
+              Container(
+                height: 200,
+                width: 250,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.yellow,
+                      Colors.yellow[300],
+                      Colors.yellow[200],
+                    ],
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.topRight,
+                  ),// Gradiente
+                ),// Box Decoration
+                child: Container(
+                  padding: EdgeInsets.only(top: 35.0, bottom: 16),
+                  child: Text(
+                    'Najera 6J',
+                    textAlign: TextAlign.center, 
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),// Texto
+                ),// Contenedor hijo
+              ),// Contenedor 2
+              SizedBox(height: 20.0,),
+              Container(
+                height: 200,
+                width: 250,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.yellow,
+                      Colors.yellow[300],
+                      Colors.yellow[200],
+                    ],
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.topRight,
+                  ),// Gradiente
+                ),// Box Decoration
+              ),// Contenedor 3
+            ],// Hijos
+          ),// Columna
+        ),// SingleChildScrollView
       ),// ConstrainedBox
     );//Material
   } // widget
