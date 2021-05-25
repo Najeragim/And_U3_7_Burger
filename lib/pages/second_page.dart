4e708f5552ba91758698e9372e7806fdf1748fed
FlutLab.io <>
2021-05-25 02:06:07 +0000

    Autosave

--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -14,39 +14,66 @@ class SecondPage extends StatelessWidget {
           height: double.infinity,
           //color: Colors.green,
           padding: EdgeInsets.symmetric(horizontal: 30.0),
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 100), 
-              Container(
-                child: Text('MC DONALD\'S',
-                  style: new TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40.0,
-                    color: Colors.orange,
-                  ),
-                ),// Texto McDonalds
-              ),// Contenedor 1
-              SizedBox(height: 40), 
-              Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(2.0),
-                    child: Stack(
-                      children: <Widget>[
-                        Align(
-                          child: new Image(
-                            width: 300.0,
-                            height: 198.12,
-                            image: new AssetImage('assets/images/burger1.png'),
-                          ),// Imagen
-                        ),// Align
-                      ],//Hijos
-                    ),// Stack
-                  ),// Contenedor
-                ],// Hijos
-              ),//Columna
-            ],// Hijos
-          ),// Columna
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 100), 
+                Container(
+                  child: Text('MC DONALD\'S',
+                    style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40.0,
+                      color: Colors.orange,
+                    ),
+                  ),// Texto McDonalds
+                ),// Contenedor 1
+                SizedBox(height: 40), 
+                Column(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(2.0),
+                      child: Stack(
+                        children: <Widget>[
+                          Align(
+                            child: new Image(
+                              width: 300.0,
+                              height: 198.12,
+                              image: new AssetImage('assets/images/burger1.png'),
+                            ),// Imagen
+                          ),// Align
+                        ],//Hijos
+                      ),// Stack
+                    ),// Contenedor
+                    SizedBox(height: 40),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Correo',
+                        icon: Icon(Icons.email_rounded),
+                        
+                      ),
+                    ),//Campo de Texto
+                    SizedBox(height: 40),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Correo',
+                        suffixIcon: Icon(Icons.email_rounded),
+                      ),
+                    ),//Campo de Texto
+                    SizedBox(height: 40),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Contraseña',
+                        suffixIcon: Icon(Icons.vpn_key_rounded),
+                        
+                      ),
+                    ),//Campo de Texto
+                  ],// Hijos
+                ),//Columna
+              ],// Hijos
+            ),// Columna
+          ),// SingleChildScrollView
         ),// Contenedor
       ),// SafeArea
     ); // Scaffold
