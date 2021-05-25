1b7212c480706bf3810e9acb8aa32bb04ec13108
FlutLab.io <>
2021-05-25 03:11:26 +0000

    Autosave

--- lib/pages/third_page.dart
+++ lib/pages/third_page.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/material.dart';
 
-class ThirdPage extends StatelessWidget {
+class ThirdPage extends StatefulWidget {
+  @override
+  _ThirdPageState createState() => _ThirdPageState();
+}
+
+const List<Color> yellowGradients = [
+  Colors.yellow,
+  Colors.yellow[300],
+  Colors.yellow[100],
+],
+
+const List<Color> orangeGradients = [
+  Colors.orange,
+  Colors.orange[300],
+  Colors.orange[100],
+],
+
+const List<Color> redGradients = [
+  Colors.red,
+  Colors.red[300],
+  Colors.red[100],
+],
+
+class _ThirdPageState extends State<ThirdPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Tercera Página',
-          style: new TextStyle(fontSize: 14.0, fontWeight: FontWeight.bold, color: Colors.black),
-        ), //Text
-      ), // center
-    ); //container
+    return Material(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 300, minWidth: 200),
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 80.0,),
+            Container(
+              color: Colors.pink,
+              height: 200,
+              width: 350,
+            ),// Contenedor 1
+          ],// Hijos
+        ),// Columna
+      ),// ConstrainedBox
+    );//Material
   } // widget
 } // Clase ThirdPage
